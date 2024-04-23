85b9f1e76732f013ee055101aed8c65247c3a608
Sohail Mirza <mirzmaster@gmail.com>
2024-04-23 17:30:34 -0400

    formatting

--- Labfiles/06-use-own-data/Python/ownData.py
+++ Labfiles/06-use-own-data/Python/ownData.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 # Add OpenAI import
 from openai import AzureOpenAI
 
-def main(): 
-        
+
+def main():
     try:
         # Flag to show citations
         show_citations = False
@@ -19,7 +19,7 @@ def main():
         azure_search_endpoint = os.getenv("AZURE_SEARCH_ENDPOINT")
         azure_search_key = os.getenv("AZURE_SEARCH_KEY")
         azure_search_index = os.getenv("AZURE_SEARCH_INDEX")
-        
+
         # Initialize the Azure OpenAI client
         client = AzureOpenAI(
             base_url=f"{azure_oai_endpoint}/openai/deployments/{azure_oai_deployment}/extensions",
@@ -30,11 +30,11 @@ def main():
         text = input('\nEnter a question:\n')
 
         # Configure your data source
-        extension_config = dict(dataSources = [
+        extension_config = dict(dataSources=[
             {
                 "type": "AzureCognitiveSearch",
                 "parameters": {
-                    "endpoint":azure_search_endpoint,
+                    "endpoint": azure_search_endpoint,
                     "key": azure_search_key,
                     "indexName": azure_search_index,
                 }
@@ -46,20 +46,20 @@ def main():
         print("Request: " + text + "\n")
 
         response = client.chat.completions.create(
-            model = azure_oai_deployment,
-            temperature = 0.5,
-            max_tokens = 1000,
-            messages = [
+            model=azure_oai_deployment,
+            temperature=0.5,
+            max_tokens=1000,
+            messages=[
                 {"role": "system", "content": "You are a helpful travel agent"},
                 {"role": "user", "content": text}
             ],
-            extra_body = extension_config
+            extra_body=extension_config
         )
 
         # Print response
         print("Response: " + response.choices[0].message.content + "\n")
 
-        if (show_citations):
+        if show_citations:
             # Print citations
             print("Citations:")
             citations = response.choices[0].message.context["messages"][0]["content"]
@@ -67,13 +67,9 @@ def main():
             for c in citation_json["citations"]:
                 print("  Title: " + c['title'] + "\n    URL: " + c['url'])
 
-
-        
     except Exception as ex:
         print(ex)
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     main()
-
-
