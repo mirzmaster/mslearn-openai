1275af42beb906ad07dea25b315c047a408acc0d
Sohail Mirza <mirzmaster@gmail.com>
2024-04-23 16:49:55 -0400

    formatting for lab 5

--- Labfiles/05-image-generation/Python/generate-image.py
+++ Labfiles/05-image-generation/Python/generate-image.py
@@ -3,21 +3,21 @@ import time
 import os
 from dotenv import load_dotenv
 
-def main(): 
-        
+
+def main():
     try:
         # Get Azure OpenAI Service settings
         load_dotenv()
         api_base = os.getenv("AZURE_OAI_ENDPOINT")
         api_key = os.getenv("AZURE_OAI_KEY")
         api_version = '2024-02-15-preview'
-        
+
         # Get prompt for image to be generated
         prompt = input("\nEnter a prompt to request an image: ")
 
         # Call the DALL-E model
         url = "{}openai/deployments/dalle3/images/generations?api-version={}".format(api_base, api_version)
-        headers= { "api-key": api_key, "Content-Type": "application/json" }
+        headers = {"api-key": api_key, "Content-Type": "application/json"}
         body = {
             "prompt": prompt,
             "n": 1,
@@ -32,12 +32,10 @@ def main():
         # Display the URL for the generated image
         print(revised_prompt)
         print(image_url)
-        
 
     except Exception as ex:
         print(ex)
 
-if __name__ == '__main__': 
+
+if __name__ == '__main__':
     main()
-
-
